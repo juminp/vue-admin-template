0c472e559dcdb627a34043a8d4771dddf3d7450d
zhenmin <zhenmin.mo@zhenai.com>
2020-09-21 16:03:39 +0800

    init

---
+++ src/store/modules/permission.js
@@ -0,0 +1,55 @@
+import { getMenu } from '@/api/user'
+import { constantRoutes } from '@/router'
+import { getToken } from '@/utils/auth'
+
+/**
+ * Use meta.role to determine if the current user has permission
+ * @param roles
+ * @param route
+ */
+// function hasPermission(roles, route) {
+//   if (route.meta && route.meta.roles) {
+//     return roles.some(role => route.meta.roles.includes(role))
+//   } else {
+//     return true
+//   }
+// }
+
+const state = {
+  routes: [],
+  addRoutes: []
+}
+
+const mutations = {
+  SET_ROUTES: (state, routes) => {
+    state.addRoutes = routes
+    state.routes = constantRoutes.concat(routes)
+  }
+}
+
+const actions = {
+  generateRoutes({ commit }) {
+    return new Promise((resolve, reject) => {
+      getMenu(getToken()).then(response => {
+        const { data } = response
+
+        if (!data) {
+          return reject('Verification failed, please Login again.')
+        }
+
+        const accessedRoutes = data
+        commit('SET_ROUTES', accessedRoutes)
+        resolve(accessedRoutes)
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
---
+++ src/views/nested/menu1/menu1-1/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div style="padding:30px;">
+    <el-alert :closable="false" title="menu 1-1" type="success">
+      <router-view />
+    </el-alert>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'Dashboard',
+  data() {
+    return {}
+  },
+  created() {
+    const id = this.$route.params && this.$route.params.id
+    // set tagsview title
+    this.setTagsViewTitle(id)
+
+    // set page title
+    this.setPageTitle(id)
+  },
+  methods: {
+    setTagsViewTitle(id) {
+      const title = '详情'
+      const route = Object.assign({}, Object.assign({}, this.$route), { title: `${title}-${id}` })
+      this.$store.dispatch('tagsView/updateVisitedView', route)
+    },
+    setPageTitle(id) {
+      const title = '详情'
+      document.title = `${title} - ${id}`
+    }
+  }
+}
+</script>
